ade6faca8e46561f306fdfac41b66e11b29f953e
danielkatsale <daniel.katsale@gmail.com>
2024-01-01 20:53:30 -0800

    Added linux x86_64 binary compilations

---
+++ bin/CMakeFiles/chip8.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/elastak/projects/cpp/projects/chip8/src/chip8.cpp" "CMakeFiles/chip8.dir/chip8.cpp.o" "gcc" "CMakeFiles/chip8.dir/chip8.cpp.o.d"
+  "/home/elastak/projects/cpp/projects/chip8/src/gfx.cpp" "CMakeFiles/chip8.dir/gfx.cpp.o" "gcc" "CMakeFiles/chip8.dir/gfx.cpp.o.d"
+  "/home/elastak/projects/cpp/projects/chip8/src/main.cpp" "CMakeFiles/chip8.dir/main.cpp.o" "gcc" "CMakeFiles/chip8.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
